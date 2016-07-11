f716267073933ef8bb41a8d5b7b5db264630e369
ajaybhardwaj7 <lookajayprofile@gmail.com>
2016-07-11 11:18:39 +0530

    'changes'

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     'secret': 'ThisIsASecretData',
-    'database': 'mongodb://localhost:27017/sampleDB'
+    'database': 'mongodb://localhost:27017/sampleDB',
+   // 'database' : 'mongodb://ajaybhardwaj7:baadshah7@ds021994.mlab.com:21994/sample'
 
 };
